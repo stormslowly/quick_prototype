bd1a6b59226feb78e2b6b237a7d3560ef4318727
stormslowly@gmail.com <stormslowly@gmail.com>
2017-09-08 02:53:30 +0800

    ✅

---
+++ src/test/registerClassMethodAsMutation.ts
@@ -0,0 +1,42 @@
+import * as chai from 'chai'
+import * as chaiProps from 'chai-properties'
+import {
+  registerField, allRegisteredTypes, registerQuery, parameterArrayOf, allQueriesGroupByClass,
+  allRegisteredTypesName, returnTypeArrayOf
+} from "../lib/types";
+import {graphqlFrom} from "../lib/typesToGraphqlSchema";
+import {allMutationsGroupByClass, registerMutation} from "../lib/types/query";
+
+const {expect} = chai
+
+chai.use(chaiProps)
+
+
+describe('class method definition', () => {
+
+
+  before(() => {
+    class TestMutation {
+      @registerField({})
+      id: string;
+
+      @registerMutation('addANewNumber')
+      @returnTypeArrayOf('Number')
+      addNumber(n: number): number[] {
+        return [n]
+      }
+    }
+  })
+
+  it('get methods definition', () => {
+    expect((allMutationsGroupByClass()['TestMutation'])).to.have.properties([
+      {
+        methodName: 'addNumber',
+        mutationName: 'addANewNumber',
+        parameters:
+          [{type: 'Number', identifier: 'n'}],
+        returnType: {name: 'Array', arrayOf: 'Number'}
+      }
+    ])
+  })
+})
